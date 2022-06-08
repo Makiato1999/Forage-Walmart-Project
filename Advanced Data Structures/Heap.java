cb1ebe6387aac4508b3a89dd2847e0675836d532
XIAORAN XIE <59595778+Makiato1999@users.noreply.github.com>
2022-06-08 18:32:32 -0500

    Updated index
    parent index

--- Advanced Data Structures/Heap.java
+++ Advanced Data Structures/Heap.java
@@ -2,15 +2,19 @@
  * Heap
  */
 public class Heap {
-    private int x = 0;
+    private double x = 0;
     private double numOfChildren = 0;
 
-    public Heap(int x) {
+    public Heap(double x) {
         this.x = x;
         this.numOfChildren = Math.pow(2, this.x);
     }
 
+    // root is index 0
     private double getLeftChildIndex(int parentIndex) {
-        return numOfChildren*parentIndex + 1;
+        return parentIndex * numOfChildren + 1;
+    }
+    private double getParentIndex(double childIndex) {
+        return (childIndex - 1) / numOfChildren;
     }
 }
